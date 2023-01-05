db78760b7782ae9afa7112f568ebfc661ce4a91f
jpcoelho78 <93714636+jpcoelho78@users.noreply.github.com>
2023-01-05 17:41:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener('scroll',
+function() {
+	
+	let pageY = window.pageYOffset;
+	let main =
+document.querySelector('main');
+main.style.backgroundPosition = `-${pageY
+* 10}px -100px`
+let main2 =
+document.querySelector('main2');
+main2.style.backgroundPosition =
+`-${pageY * 5}px -100px`
+
+});
+window.onscroll = function() {
+        var theta = document.documentElement.scrollTop / 50 % Math.PI;
+        var poxy = document.documentElement.scrollTop/20;
+
+
+		document.getElementById('garrafa').style.transform ='rotate(' + theta + 'rad)';
+
+		document.getElementById('garrafa').style.top =poxy+'px';
+
+}
